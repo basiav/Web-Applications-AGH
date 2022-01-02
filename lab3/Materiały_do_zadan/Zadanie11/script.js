fe618ef2dbd5b79f64b874de038b442063fbd4e2
basiav <82549088+basiav@users.noreply.github.com>
2022-01-02 01:51:17 +0100

    Ex.11

--- lab3/Materiały_do_zadan/Zadanie11/script.js
+++ lab3/Materiały_do_zadan/Zadanie11/script.js
@@ -11,17 +11,44 @@ enemyImage.src = 'images/walkingdead.png';
 const numberOfFrames = 10;
 let gameFrame = 0;
 
+const scorePosition = [200, 200]; 
+// console.log("---------DIMS---------");
+// // var rect = canvas.getBoundingClientRect();
+// var rect = document.body.getBoundingClientRect();
+// console.log(rect.top, rect.right, rect.bottom, rect.left);
+// var bodyRect = document.body.getBoundingClientRect(),
+//     elemRect = element.getBoundingClientRect(),
+//     offset   = elemRect.top - bodyRect.top;
+
+// alert('Element is ' + offset + ' vertical pixels from <body>');
+
+class VisualProperties {
+    static spriteWidth = parseInt(enemyImage.width / numberOfFrames);
+    static spriteHeight = parseInt(enemyImage.height);
+    static scale = 2.5;
+}
+
+// OLD CODE
+// this.spriteWidth = parseInt(enemyImage.width / numberOfFrames);
+// this.spriteHeight = parseInt(enemyImage.height);
+// this.width = this.spriteWidth / 2.5;
+// this.height = this.spriteHeight / 2.5;
 class Enemy {
     constructor(){
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.speed = Math.random() * 4 - 2;
-        this.spriteWidth = parseInt(enemyImage.width / numberOfFrames);
-        this.spriteHeight = parseInt(enemyImage.height);
-        this.width = this.spriteWidth / 2.5;
-        this.height = this.spriteHeight / 2.5;
+        this.setVisualProperties();
         this.frame = 0;
-        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.flapSpeed = Math.floor(Math.random() * 5 + 1);
+    }
+
+    setVisualProperties(){
+        var vp = VisualProperties;
+        this.spriteWidth = vp.spriteWidth;
+        this.spriteHeight = vp.spriteHeight;
+        this.width = this.spriteWidth / vp.scale;
+        this.height = this.spriteHeight / vp.scale;
     }
 
     update(){
@@ -43,6 +70,22 @@ for(let i = 0; i < numberOfEnemies; i++){
     enemiesArray.push(new Enemy());
 }
 
+var score = 0;
+const scoreX = scorePosition[0], scoreY = scorePosition[1];
+
+function drawScore() {
+    ctx.font = "35px Arial";
+    ctx.fillStyle = "#FFFFFF";
+    // ctx.shadowColor =  "#ab58ac";
+    ctx.shadowColor =  "#400d40";
+    ctx.shadowBlur = 10;
+    ctx.lineWidth = 5;
+    ctx.strokeText("Score: " + score, scoreX, scoreY);
+    ctx.shadowBlur = 0;
+    ctx.fillText("Score: " + score, scoreX, scoreY);
+    ctx.shadowBlur = 10;
+}
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     enemiesArray.forEach(enemy => {
@@ -51,7 +94,10 @@ function animate(){
     });
     gameFrame = (gameFrame % 100) + 1;
 
+    drawScore();
+
     requestAnimationFrame(animate);
 }
 
 animate();
+
