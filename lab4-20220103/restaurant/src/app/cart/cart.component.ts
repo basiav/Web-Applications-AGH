92ffee682b3ce9ecf01a32db0e1a487ed1561f1b
basiav <82549088+basiav@users.noreply.github.com>
2022-02-12 19:14:05 +0100

    Cart ready

--- lab4-20220103/restaurant/src/app/cart/cart.component.ts
+++ lab4-20220103/restaurant/src/app/cart/cart.component.ts
@@ -18,4 +18,16 @@ export class CartComponent implements OnInit {
     this.items = this.cartService.getCartItems();
   }
 
+  getCartValue(): number {
+    // return this.items.filter(item => item.tax === '25.00')
+    // .reduce((sum, current) => sum + current.total, 0);
+    let acc: number = 0;
+    this.items.forEach((quantity, dish) => {
+      if (dish) {
+        acc += quantity * dish.price;
+      }
+    });
+    return acc;
+  }
+
 }
