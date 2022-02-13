996b2c74e20a108cf0a0c6040c761685d56d12d3
basiav <82549088+basiav@users.noreply.github.com>
2022-02-13 01:24:16 +0100

    Esthetic changes

--- lab4-20220103/restaurant/src/app/dishes/dish-search/dish-search.component.ts
+++ lab4-20220103/restaurant/src/app/dishes/dish-search/dish-search.component.ts
@@ -11,8 +11,13 @@ import { Dish } from '../../shared/dish';
 export class DishSearchComponent implements OnInit {
   dishes$!: Observable<Dish[]>;
   private searchTerms = new Subject<string>();
+  searchCategories: String[];
+  selectedCategory?: string;
 
-  constructor(private searchService: SearchService) { }
+
+  constructor(private searchService: SearchService) { 
+    this.searchCategories = ["Typ kuchni", "Cena", "Ocena", "Kategoria dania"];
+  }
 
   search(term: string): void {
     this.searchTerms.next(term);
@@ -20,8 +25,8 @@ export class DishSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.dishes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+      // wait 200ms after each keystroke before considering the term
+      debounceTime(200),
 
       // ignore new term if same as previous term
       distinctUntilChanged(),
@@ -30,5 +35,50 @@ export class DishSearchComponent implements OnInit {
       switchMap((term: string) => this.searchService.searchDishes(term)),
     );
   }
+
+  // carsAvailable: Car[] = []; 
+  // selectedBrand?: string;
+  // selectedModel?: string;
+  // selectedColour?: Colour;
+
+  // constructor(private carService: CarService) { }
+
+  // ngOnInit(): void {
+  //   this.getCars();
+  // }
+
+  // getCars(): void {
+  //   this.carService.getCars()
+  //   .subscribe(cars => this.carsAvailable = cars);
+  // }
+
+  // getDistinctBrands(): string[] {
+  //   return this.carsAvailable.map(car => car.brand)
+  //   .filter((value, index, self) => self.indexOf(value) === index)
+  // }
+
+  // getDistinctModels(): string[] {
+  //   console.log("Selected brand: ", this.selectedBrand);
+  //   console.log(this.carsAvailable
+  //     .filter(car => car.brand === this.selectedBrand)
+  //     .map(car => car.model)
+  //     .filter((value, index, self) => self.indexOf(value) === index))
+
+  //   return this.carsAvailable
+  //   .filter(car => car.brand === this.selectedBrand)
+  //   .map(car => car.model)
+  //   .filter((value, index, self) => self.indexOf(value) === index)
+  // }
+
+  // getDistinctColours(): Colour[] {
+  //   console.log("Selected model: ", this.selectedModel);
+  //   console.log(this.carsAvailable
+  //   .filter(car => (car.brand === this.selectedBrand && car.model == this.selectedModel))
+  //   .map(car => car.colours)[0])
+
+  //   return this.carsAvailable
+  //   .filter(car => (car.brand === this.selectedBrand && car.model == this.selectedModel))
+  //   .map(car => car.colours)[0]
+  // }
 
 }
