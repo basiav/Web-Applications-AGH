ec374434501153dbd56ec502810ea335680709cf
basiav <82549088+basiav@users.noreply.github.com>
2022-02-22 18:56:40 +0100

    Sligh change

--- lab6/RestaurantApp/api/routes/privateRouteEx.js
+++ lab6/RestaurantApp/api/routes/privateRouteEx.js
@@ -2,12 +2,7 @@ const router = require('express').Router();
 const verify = require('./verifyToken');
 
 router.get('/', verify, (req, res) => {
-    res.json({
-        posts: {
-            title: "Top secret post",
-            description: "You have to be logged in to see this conent!"
-        }
-    })
+    res.send(req.user);
 });
 
 module.exports = router;
